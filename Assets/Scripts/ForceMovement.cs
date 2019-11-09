236336fe17b56e4fad4dbcba9f660146cb8f0559
Thomas Sullivan <tdsullivan812@gmail.com>
2019-11-09 16:22:24 -0500

    Most likely irrelevant now, but includes a very simple WASD controller.

---
+++ Assets/Scripts/ForceMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceMovement : MonoBehaviour
+{
+    Vector3 deltaX;
+    Vector3 deltaZ;
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 position = transform.position;
+        deltaX = Vector3.right;
+        deltaZ = Vector3.forward;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            rigidbody.AddForce(-10*deltaX);
+        }
+        if (Input.GetKey("d"))
+        {
+            rigidbody.AddForce(10*deltaX);
+        }
+        if (Input.GetKey("w"))
+        {
+            rigidbody.AddForce(deltaZ);
+        }
+        if (Input.GetKey("s"))
+        {
+            rigidbody.AddForce(-deltaZ);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 deltaX;
+    Vector3 deltaZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 position = transform.position;
+        deltaX = Vector3.right;
+        deltaZ = Vector3.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.position = transform.position - deltaX * (0.1f);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position = transform.position + deltaX * 0.1f;
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.position = transform.position + deltaZ * (0.1f);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position = transform.position - deltaZ * (0.1f);
+        }
+    }
+}
